fa0620b955fe8435b5d6fb3814c6eb1b099f30d8
GSPQQ <dannavolwo@gmail.com>
2026-05-19 15:37:38 +0400

    Update Program.cs

--- laba3/laba3/Program.cs
+++ laba3/laba3/Program.cs
@@ -2,18 +2,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-// Register exception filter types for ServiceFilter usage
+
 builder.Services.AddScoped<laba3.Filters.HandleErrorAttribute>();
 builder.Services.AddScoped<laba3.Filters.FormatExceptionFilterAttribute>();
 builder.Services.AddScoped<laba3.Filters.PostIndexActionFilter>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
