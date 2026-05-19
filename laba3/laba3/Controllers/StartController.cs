f10ae9ca25ed1e932053ac2bc37d5cd0146fc699
GSPQQ <dannavolwo@gmail.com>
2026-05-19 15:36:41 +0400

    Update StartController.cs

--- laba3/laba3/Controllers/StartController.cs
+++ laba3/laba3/Controllers/StartController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace laba3.Controllers
 {
-    // Entry point for CustomIController.Execute (action=start, id=0 → Simple/Index).
+    
     public class StartController : Controller
     {
         public IActionResult Start(int id = 0)
